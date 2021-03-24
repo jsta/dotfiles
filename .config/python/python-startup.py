4c486d25c5ae5fd829e7247ba2b23f46c401f4ca
jsta <stachel2@msu.edu>
2021-03-24 14:02:24 -0400

    print all pandas rows

--- .config/python/python-startup.py
+++ .config/python/python-startup.py
@@ -1,5 +1,6 @@
 try:
     import pandas as pd
     pd.set_option("display.max_columns", None)
+    pd.set_option('display.max_rows', None)
 except:
     print("No pandas installation found to set options.")
